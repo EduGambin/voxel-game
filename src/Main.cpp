73417e045fff0328b2afc3c977d0d38c329bb929
Edu <gambinmonserratedu@gmail.com>
2019-11-06 11:19:15 +0100

    Basic window rendering

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,7 +1,61 @@
 #include <iostream>
+#include "glad/glad.h"
+#include <GLFW/glfw3.h>
+
+#define SCREEN_WIDTH 1280
+#define SCREEN_HEIGHT 720
 
 int main()
 {
-    std::cout << "Hello world!" << std::endl;
-    return 0;
+	// Initializing GLFW.
+	glfwInit();
+
+	// Setting the OpenGL version.
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Setting the window options.
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+	glfwWindowHint(GLFW_FOCUSED, GLFW_TRUE);
+	glfwWindowHint(GLFW_DOUBLEBUFFER, GLFW_TRUE);
+
+	// Creating the window.
+	GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Minecraft", nullptr, nullptr);
+	if (window == nullptr)
+	{
+		std::cerr << "Failed to create the GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// Initializing GLAD.
+	if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress))
+	{
+		std::cerr << "Failed to initialize GLAD" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	// Main loop.
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+		if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		{
+			glfwSetWindowShouldClose(window, GLFW_TRUE);
+		}
+
+		// Cleaning the screen.
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// Presenting the graphics.
+		glfwSwapBuffers(window);
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
 }
